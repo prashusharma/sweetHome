804cac7a34832876f5895a2d49139c84645f7759
prashusharma <prashukumarsharmassm2000@gmail.com>
2022-02-17 21:43:14 +0530

    added some files and modified some

---
+++ controller/userController.js
@@ -0,0 +1,77 @@
+const userModel = require("../DataBase/model/userSchema");
+const bcrypt = require("bcrypt");
+
+const userController = {
+  createUser: async (req, res) => { 
+    const salt = await bcrypt.genSalt();   //generating salt
+
+    let password = req.body.password;
+    let confirmPassword = req.body.confirmPassword;
+
+    if(password != confirmPassword){   //Checking password and confirm password
+      confirmPassword = '';
+    }else{
+      confirmPassword = "confirmed";
+    }
+
+    if (password != '') {       //Hashing Password
+      password = await bcrypt.hash(req.body.password, salt);
+    } 
+    const newUser = new userModel({
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          mobile: req.body.mobile,
+          email: req.body.email,
+          password,
+          confirmPassword
+    });
+    
+    try {
+        const savedUser = await newUser.save();   //saving user to dataBase
+        res.cookie('userId',savedUser._id, { maxAge: 900000, httpOnly: true }); //setting cookies
+        res.status("200").json({"message": "success"});
+
+    } 
+    catch (err) {
+      const sendErrArray = {};
+      sendErrArray["message"] = "error";
+
+      Object.values(err.errors).forEach(element => { 
+        sendErrArray[element.properties.path] = element.properties.message;
+      });
+
+      res.status("200").json(sendErrArray);
+      
+    }
+  },
+
+  loginUser : async (req, res)=>{
+    const user = await userModel.findOne({email : req.body.email});
+    const sendErrArray = {};
+    if(user){
+      const result = await bcrypt.compare(req.body.password, user.password);
+      if(result){
+
+        res.json({"message" : "success"})
+      }
+      else{
+        res.json({"message" : "Either email or password is incorrect"})
+      }
+    }
+    else{
+      res.json({"message" : "Either email or password is incorrect"})
+    }
+    
+    
+  }
+
+
+
+
+
+
+
+
+};
+
+module.exports = userController;
--- server.js
+++ server.js
@@ -1,17 +1,29 @@
+require('dotenv').config()
 const express = require("express");
 const app = express();
 const routes = require("./Routes/getRoute");
+const regRoutes = require("./Routes/postRoute");
+const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser");
+require("./DataBase/dbConnection");
 // const path = 
 const port = process.env.PORT || 3000;
 
-app.use( express.static('static'))
+//Body- Parser
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.json());
+app.use(cookieParser());
 // app.use( express.static('static'))
+app.use( express.static('static'))
 
 //Router SetUp
 app.use(routes); 
+app.use(regRoutes); 
 
 //view engine
 app.set("view engine", "ejs")
+
+
 
 app.listen(port, ()=> {
     console.log("server started");
